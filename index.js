2f4d6b4f124bf61a38134e18281d21ce61c28be1
Bobby Ishak <bobbyishak@yahoo.com>
2020-05-04 09:51:06 +0700

    Mbuat update profile
    Nambah JWT, update profile

--- index.js
+++ index.js
@@ -1,9 +1,52 @@
 const express = require('express');
 const mysql = require('mysql');
+const jwt = require('jsonwebtoken');
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.urlencoded({urlencoded: true}));
+const conn = mysql.createPool({
+    database: "soa_proyek_db",
+    user: "root",
+    password: "",
+    host: "localhost"
+});
+
+executeQuery = (conn, query) => {
+    return new Promise((resolve, reject) => {
+        conn.query(query, (err, result) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(result);
+            }
+        });
+    });
+};
+
+
+
+app.put("/api/update_profile/:email", async function (req,res) {
+    let email = req.params.email;
+    let password = req.body.password;
+    let address = req.body.address;
+    let phone = req.body.phone;
+    let name = req.body.name;
+
+    if (!email) {
+        return res.status(400).send("No email reference!");
+    }
+
+    let checkUser = await executeQuery(conn, `select * from user where user_email = '${email}'`);
+    if (checkUser.length < 1) {
+        return res.status(400).send("User with that email doesn't exist!");
+    }
+
+    let updateEmail = await executeQuery(conn, `update user set user_password = '${password}', user_address = '${address}', user_phone = '${phone}', user_name = '${name}' where user_email = '${email}'`);
+    if(updateEmail["affectedRows"] > 0){
+        return res.status(200).send("Account berhasil diubah!");
+    }
+});
 
 app.listen(3000, function (req,res) {
     console.log("Listening on port 3000...");
