376cd5621acd80d0dd85ed3e3e33c8973b04c50d
protex121 <protex121@users.noreply.github.com>
2020-05-05 21:03:25 +0700

    selesaikan watchlish

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const jwt = require('jsonwebtoken');
 const hash = require('./hash_string');
+const request = require('request');
 
 const app = express();
 
@@ -50,11 +51,11 @@ app.post("/api/register", async (req,res)=>{
   if(!user_email||!user_password||!user_address||!user_phone||user_name) return res.status(400).send("semua field harus diisi")
   let query = `INSERT INTO user VALUES('${user_email}','${user_password}',${user_balance},'${user_key}','${user_address}','${user_phone}','${user_name}', NOW() + INTERVAL 7 DAY)`;
   let conn = await getConnection();
-  
+
   try {
     let result = await executeQuery(conn, query);
     conn.release();
-  
+
   } catch (error) {
       console.log("error : " +error)
       return res.status(400).send("email sudah terdaftar!")
@@ -113,18 +114,65 @@ app.post("/api/login",async function(req,res){
     const conn = await getConnection()
     const email = req.body.email
     const password = req.body.password
-    const key = req.body.key 
+    const key = req.body.key
     let que = `SELECT * FROM user WHERE user_email = '${email}' and user_password = '${password}'`
     const user = await executeQuery(conn,que)
     if(user.length == 0) return res.status(400).send({status:400,message:"email or password incorrect!"})
-    
+
     if(user[0].user_key != key) return res.status(400).send({status:400,message:"key invalid!"})
-    
+
     return res.status(200).send({status:200,message:"login successful!"})
   })
 
+app.post("/api/addWatchlist", async (req,res)=>{
+  let email_user = req.body.user_email;
+  let movie_id = req.body.movie_id;
+
+  let query = `INSERT INTO watchlist VALUES('${email_user}','${movie_id}')`;
+  let conn = await getConnection();
+  let result = await executeQuery(conn, query);
+  conn.release();
+
+  res.status(200).send("Add to Watchlist");
+});
+
+app.get("/api/watchlist",async (req,res)=>{
+  let user_email = req.query.user;
+  let query = `SELECT movie_id FROM watchlist WHERE email_user='${user_email}'`;
+  let conn = await getConnection();
+  let result = await executeQuery(conn, query);
+  conn.release();
+  if(Object.keys(result).length == 0) return res.status(200).send("anda belum memiliki watchlist");
+
+  res.status(200).send(result);
+});
+
+app.delete("/api/deleteWatchlist",async (req,res)=>{
+  let email_user = req.body.user_email;
+  let movie_id = req.body.movie_id;
+
+  let query = `DELETE FROM watchlist WHERE movie_id='${movie_id}' AND email_user='${email_user}'`;
+  let conn = await getConnection();
+  let result = await executeQuery(conn, query);
+  conn.release();
+
+  res.status(200).send("Delete From Watchlist");
+});
+
+app.get("/api/search/movies",async (req,res)=>{
+  var keyword = req.body.keyword;
+  var options = {
+    'method': 'GET',
+    'url': `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&query=${keyword}`,
+  };
+
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    res.status(200).send(response.body);
+  });
+});
+
+
 
 //listener
-app.listen(3000, function (req,res) {
-    console.log("Listening on port 3000...");
-});
+app.listen(3000, function (req,res) { console.log("Listening on port 3000..."); });
